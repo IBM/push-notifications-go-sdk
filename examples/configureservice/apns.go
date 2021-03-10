95fd8a3fb2bbbe330c51eb6320bf0d9589ee823c
Pankaj <pankk217@in.ibm.com>
2021-03-10 13:48:01 +0530

    initial commit

---
+++ examples/configureservice/apns.go
@@ -0,0 +1,78 @@
+package configureservice
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+
+	"github.com/IBM/push-notifications-go-sdk/pushservicev1"
+)
+
+// GetApnsConfiguration gets apns configuration
+func GetApnsConfiguration(appID string, app *pushservicev1.PushServiceV1) {
+
+	result, response, err := app.GetApnsConf(&pushservicev1.GetApnsConfOptions{
+		ApplicationID: &appID,
+	})
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if response.StatusCode == 200 {
+		fmt.Println(*result.Certificate)
+		fmt.Println(*result.IsSandBox)
+		fmt.Println(*result.ValidUntil)
+	}
+}
+
+// SaveApnsConfiguration saves apns configuration
+func SaveApnsConfiguration(appID string, app *pushservicev1.PushServiceV1) {
+	isSandBox := false
+	password := ""
+
+	fileDir, _ := os.Getwd()
+	fileName := ""
+	filePath := path.Join(fileDir, fileName)
+
+	file, _ := os.Open(filePath)
+	defer file.Close()
+
+	certiificateData := ioutil.NopCloser(file)
+	contentType := "multipart/form-data"
+
+	result, response, err := app.SaveApnsConf(&pushservicev1.SaveApnsConfOptions{
+		ApplicationID:          &appID,
+		Password:               &password,
+		IsSandBox:              &isSandBox,
+		Certificate:            certiificateData,
+		CertificateContentType: &contentType,
+	})
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if response.StatusCode == 200 {
+		fmt.Println(*result.Certificate)
+		fmt.Println(*result.IsSandBox)
+		fmt.Println(*result.ValidUntil)
+	}
+}
+
+// DeleteApnsConfiguration deletes apns configuration
+func DeleteApnsConfiguration(appID string, app *pushservicev1.PushServiceV1) {
+
+	response, err := app.DeleteApnsConf(&pushservicev1.DeleteApnsConfOptions{
+		ApplicationID: &appID,
+	})
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if response.StatusCode == 204 {
+		fmt.Println("Deleted")
+	}
+}
---
+++ examples/configureservice/gcm.go
@@ -0,0 +1,79 @@
+package configureservice
+
+import (
+	"fmt"
+
+	"github.com/IBM/push-notifications-go-sdk/pushservicev1"
+)
+
+// GetGcmConfiguration gets gcm configuration
+func GetGcmConfiguration(appID string, app *pushservicev1.PushServiceV1) {
+
+	result, response, err := app.GetGCMConf(&pushservicev1.GetGCMConfOptions{
+		ApplicationID: &appID,
+	})
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if response.StatusCode == 200 {
+		fmt.Println(*result.ApiKey)
+		fmt.Println(*result.SenderID)
+	}
+}
+
+// SaveGcmConfiguration saves gcm configuration
+func SaveGcmConfiguration(appID string, app *pushservicev1.PushServiceV1) {
+	apiKey := ""
+	senderID := ""
+	result, response, err := app.SaveGCMConf(&pushservicev1.SaveGCMConfOptions{
+		ApplicationID: &appID,
+		ApiKey:        &apiKey,
+		SenderID:      &senderID,
+	})
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if response.StatusCode == 200 {
+		fmt.Println(*result.ApiKey)
+		fmt.Println(*result.SenderID)
+	}
+}
+
+// DeleteGcmConfiguration deletes gcm configuration
+func DeleteGcmConfiguration(appID string, app *pushservicev1.PushServiceV1) {
+
+	response, err := app.DeleteGCMConf(&pushservicev1.DeleteGCMConfOptions{
+		ApplicationID: &appID,
+	})
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if response.StatusCode == 204 {
+		fmt.Println("Deleted")
+	}
+}
+
+// GetGcmConfigurationPublic get gcm configuration public
+func GetGcmConfigurationPublic(appID string, clientSecret string, app *pushservicev1.PushServiceV1) {
+
+	result, response, err := app.GetGcmConfPublic(&pushservicev1.GetGcmConfPublicOptions{
+		ApplicationID: &appID,
+		ClientSecret:  &clientSecret,
+	})
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if response.StatusCode == 200 {
+		if response.StatusCode == 200 {
+			fmt.Println(*result.SenderID)
+		}
+	}
+}
